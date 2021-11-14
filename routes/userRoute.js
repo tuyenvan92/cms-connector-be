7c0a1926941e577549576a1893dab73a4162eafb
Tuyen Van <emux189@gmail.com>
2021-11-13 22:30:59 -0700

    m

--- routes/userRoute.js
+++ routes/userRoute.js
@@ -1,5 +1,9 @@
 const router = require('express').Router();
 
+const User = require('../models/userModel')
+
+const bcrypt = require('bcrypt')
+
 // @route         GET api/user
 // @description   GET User List
 router.get('/', (_,res) => {
@@ -73,8 +77,54 @@ router.get('/', (_,res) => {
 // @route         GET api/user/:id
 // @description   GET single user
 router.get('/:id', (req,res) => {
-    console.loh('single user:', req.params.id)
+    console.log('single user:', req.params.id)
     res.send('get single user')
+})
+
+// @route     POST/api/user/register
+// @desc      Register/User
+// @access    Public
+router.post('/register', async (req,res) => {
+  console.log('register: ', req.body)
+  const email = req.body.email
+  const password = req.body.password
+  const firstName = req.body?.firstName || ''
+  const lastName = req.body?.lastName || ''
+
+  // check email exists
+
+  const isEmailExists = await User.findOne({ email: email })
+  console.log('emaillll',email)
+
+  if (isEmailExists) return res.status(400).json({
+    msg: 'Email already exists',
+    isSuccess: false
+  })
+
+  // hash password
+  const salt = await bcrypt.genSalt(10)
+  const hashPassword = await bcrypt.hash(password, salt)
+
+  // create a new user
+  const user = new User({
+    firstName,
+    lastName,
+    email,
+    password: hashPassword
+  })
+
+  try {
+    await user.save()
+    res.json({
+      msg: 'Register successfully!',
+      isSuccess: true
+    })
+  } catch (err) {
+    res.status(400).json({
+      msg:err,
+      isSuccess: false
+    })
+  }
 })
 
 module.exports = router;
